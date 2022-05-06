2bd9729de345e3dd947f59a3b6433629ed65224d
LilPessy <pettidaniele1f@gmail.com>
2022-05-07 00:35:30 +0200

    aggiunto tutto il drum kit
    SIUM

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,79 @@ let drum = [
         name: 'open-hat',
         src: 'assets/sounds/openedHat.wav',
         img: 'assets/img/closedHat.png',
+    },
+
+    {
+        id: 5,
+        name: 'campanaccio',
+        src: 'assets/sounds/campanaccio.mp3',
+        img: 'assets/img/campanaccio.png',
+    },
+
+    {
+        id: 6,
+        name: 'rimshot',
+        src: 'assets/sounds/rimshot.wav',
+        img: 'assets/img/rimshot.png',
+    },
+
+    {
+        id: 7,
+        name: 'bacchettine',
+        src: 'assets/sounds/bacchettine.mp3',
+        img: 'assets/img/bacchettine.png',
+    },
+
+    {
+        id: 8,
+        name: 'tom-basso',
+        src: 'assets/sounds/tom_.mp3',
+        img: 'assets/img/tom_.webp',
+    },
+
+ {
+        id: 9,
+        name: 'metronomo',
+        src: 'assets/sounds/metronomo.mp3',
+        img: 'assets/img/metronomo.webp',
+    },
+ 
+ {      id: 10,
+        name: 'clap',
+        src: 'assets/sounds/clap.wav',
+        img: 'assets/img/clap.png',
+    },
+ 
+ {      id: 11,
+        name: 'crash',
+        src: 'assets/sounds/crash.wav',
+        img: 'assets/img/crash.webp',
+    },
+
+ {      id: 12,
+        name: 'ride',
+        src: 'assets/sounds/ride.wav',
+        img: 'assets/img/ride.png',
+    },
+ 
+ {      id: 13,
+        name: 'splashh',
+        src: 'assets/sounds/splashh.wav',
+        img: 'assets/img/splashh.png',
+    },
+ 
+ {      id: 14,
+        name: 'ridebell',
+        src: 'assets/sounds/ridebell.wav',
+        img: 'assets/img/ridebell.webp',
+    },
+ 
+ {      id: 15,
+        name: 'pedale-hat',
+        src: 'assets/sounds/pedale-hat.wav',
+        img: 'assets/img/pedale-hat.webp',
     }
+ 
 ];
 
 
